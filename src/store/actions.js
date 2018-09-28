8ce70571a7e609375f9284d04305a699a2484694
happyu <290945266@qq.com>
2018-09-28 17:29:53 +0800

    refactor: 添加必要注释

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,8 +2,12 @@ import * as types from './mutation-types'
 import api from '../api'
 
 export default {
+  // 根据分类id获取商品列表
   async getCategoryGoods ({ commit, state }, categoryId) {
+    // 标示是否正在加载数据中
     commit(types.SET_ISLOADING, true)
+    // 主要是用于优化 如果categoryGoodsBackup中已存在之前加载的数据
+    // 那么直接从store中获取， 不需要再去请求接口
     if (state.categoryGoodsBackup.length > 0) {
       for (let i = 0; i < state.categoryGoodsBackup.length; i++) {
         if (state.categoryGoodsBackup[i][categoryId]) {
